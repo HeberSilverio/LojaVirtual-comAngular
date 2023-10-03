c4dd3c4c911f1fd3c31817bf796c787dc50e54f3
Heber Costa <heber.costa@earqconsultoria.com>
2023-10-03 09:11:14 -0300

    Corrigido o erro de rotas - Conclusão dos trabalhos do dia 02-10-2023

--- loja-online/src/app/app.module.ts
+++ loja-online/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SellerAuthComponent } from './seller-auth/seller-auth.component';
     SellerAuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
